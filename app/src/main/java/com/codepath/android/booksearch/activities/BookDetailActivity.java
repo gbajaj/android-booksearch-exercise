4bb60182eadce026effeada369c713d67e9539b1
Eric S <eric@es0329.com>
2017-03-15 22:09:43 -0400

    Setup BookDetailActivity

--- app/src/main/java/com/codepath/android/booksearch/activities/BookDetailActivity.java
+++ app/src/main/java/com/codepath/android/booksearch/activities/BookDetailActivity.java
@@ -6,48 +6,51 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.codepath.android.booksearch.R;
+import com.codepath.android.booksearch.models.Book;
+import com.squareup.picasso.Picasso;
 
 public class BookDetailActivity extends AppCompatActivity {
-    private ImageView ivBookCover;
-    private TextView tvTitle;
-    private TextView tvAuthor;
+  private ImageView ivBookCover;
+  private TextView tvTitle;
+  private TextView tvAuthor;
+  private Book book;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_book_detail);
-        // Fetch views
-        ivBookCover = (ImageView) findViewById(R.id.ivBookCover);
-        tvTitle = (TextView) findViewById(R.id.tvTitle);
-        tvAuthor = (TextView) findViewById(R.id.tvAuthor);
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_book_detail);
 
-        // Extract book object from intent extras
+    // Fetch views
+    ivBookCover = (ImageView) findViewById(R.id.ivBookCover);
+    tvTitle = (TextView) findViewById(R.id.tvTitle);
+    tvAuthor = (TextView) findViewById(R.id.tvAuthor);
 
-        // Use book object to populate data into views
+    // Extract book object from intent extras
+    book = getIntent().getParcelableExtra("book");
+
+    // Use book object to populate data into views
+    Picasso.with(this).load(book.getCoverUrl()).into(ivBookCover);
+    tvTitle.setText(book.getTitle());
+    tvAuthor.setText(book.getAuthor());
+  }
+
+  @Override public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_book_detail, menu);
+    return true;
+  }
+
+  @Override public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_book_detail, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+    return super.onOptionsItemSelected(item);
+  }
 }
