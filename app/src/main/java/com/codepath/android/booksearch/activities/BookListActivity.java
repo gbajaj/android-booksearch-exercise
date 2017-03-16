a95d3777da55dc5c1bf7737ed32cf345899fdf9f
Eric S <eric@es0329.com>
2017-03-15 22:23:32 -0400

    Implement listener in book list activity

--- app/src/main/java/com/codepath/android/booksearch/activities/BookListActivity.java
+++ app/src/main/java/com/codepath/android/booksearch/activities/BookListActivity.java
@@ -8,7 +8,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.Toast;
-
 import com.codepath.android.booksearch.R;
 import com.codepath.android.booksearch.adapters.BookAdapter;
 import com.codepath.android.booksearch.models.Book;
@@ -28,8 +27,14 @@ public class BookListActivity extends AppCompatActivity implements BookAdapter.L
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_book_list);
+    // Find the toolbar view inside the activity layout
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    // Sets the Toolbar to act as the ActionBar for this Activity window.
+    // Make sure the toolbar exists in the activity and is not null
+    setSupportActionBar(toolbar);
     lvBooks = (ListView) findViewById(R.id.lvBooks);
     ArrayList<Book> aBooks = new ArrayList<>();
+
     // initialize the adapter
     bookAdapter = new BookAdapter(this, aBooks);
     // attach the adapter to the ListView
@@ -72,81 +77,30 @@ public class BookListActivity extends AppCompatActivity implements BookAdapter.L
     });
   }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_book_list);
-        // Find the toolbar view inside the activity layout
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        // Sets the Toolbar to act as the ActionBar for this Activity window.
-        // Make sure the toolbar exists in the activity and is not null
-        setSupportActionBar(toolbar);
-        lvBooks = (ListView) findViewById(R.id.lvBooks);
-        ArrayList<Book> aBooks = new ArrayList<>();
+  @Override public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_book_list, menu);
+    return true;
+  }
 
-        // initialize the adapter
-        bookAdapter = new BookAdapter(this, aBooks);
-        // attach the adapter to the ListView
-        lvBooks.setAdapter(bookAdapter);
-        // Fetch the data remotely
-        fetchBooks("Oscar Wilde");
-
-    }
-
-    // Executes an API call to the OpenLibrary search endpoint, parses the results
-    // Converts them into an array of book objects and adds them to the adapter
-    private void fetchBooks(String query) {
-        client = new BookClient();
-        client.getBooks(query, new JsonHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                try {
-                    JSONArray docs;
-                    if (response != null) {
-                        // Get the docs json array
-                        docs = response.getJSONArray("docs");
-                        // Parse json array into array of model objects
-                        final ArrayList<Book> books = Book.fromJson(docs);
-                        // Remove all books from the adapter
-                        bookAdapter.clear();
-                        // Load model objects into the adapter
-                        for (Book book : books) {
-                            bookAdapter.add(book); // add book through the adapter
-                        }
-                        bookAdapter.notifyDataSetChanged();
-                    }
-                } catch (JSONException e) {
-                    // Invalid JSON format, show appropriate error.
-                    e.printStackTrace();
-                }
-            }
+  @Override public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
 
     //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
+    if (id == R.id.actionSearch) {
+      Toast.makeText(this, "Search Begins", Toast.LENGTH_SHORT).show();
       return true;
     }
 
     return super.onOptionsItemSelected(item);
   }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.actionSearch) {
-            Toast.makeText(this, "Search Begins", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
   @Override public void onBookItemClick(Book book) {
     Intent intent = new Intent(this, BookDetailActivity.class);
-    intent.putExtra("book", book);
+    //intent.putExtra("book", book);
     startActivity(intent);
   }
 }
